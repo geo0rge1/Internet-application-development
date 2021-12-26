392aac5bde03096776b483f9b6ed6b71edf0d9e8
geo0rge1 <baoz9912@gmail.com>
2021-12-26 19:56:28 +0300

    lab7

---
+++ LAB7/lb7/src/components/stock/StockComponent.js
@@ -0,0 +1,27 @@
+export class StockComponent {
+    constructor(parent) {
+        this.parent = parent
+    }
+
+    getHTML(data) {
+        return (
+            `
+                <div class="card mb-3" style="width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.name}</h5>
+                                <p class="card-text">${data.text}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        )
+    }
+
+    render(data) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+    }
+}
