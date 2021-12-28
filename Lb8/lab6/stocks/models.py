fbc66e0fc1bf748b3b7c664f978b6ced32caf095
geo0rge1 <baoz9912@gmail.com>
2021-12-28 16:51:46 +0300

    lab8

--- Lb8/lab6/stocks/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Stock(models.Model):
-    namestock = models.CharField(max_length=50, verbose_name="Название компании")
-    description = models.CharField(max_length=50, verbose_name="описание компании")
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена акции")
